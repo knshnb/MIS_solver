c860f4d1ab060e374c630bd2703b45a297505616
Kenshin Abe <abe.kenshin@gmail.com>
2019-05-21 17:59:42 +0900

    No parallelization

--- mcts/mcts_node.py
+++ mcts/mcts_node.py
@@ -1,4 +1,3 @@
-from multiprocessing import Pool
 import copy
 import numpy as np
 import torch
@@ -46,14 +45,14 @@ class MCTSNode:
                 self.reward_mean, self.reward_std = NodeHash.get(hash)
             else:
                 # ランダム試行でrewardの平均、分散を求める
-                NUM = min(max(100, 2 * n), 500)
+                # 試行回数のMAXは100に固定
+                NUM = min(max(10, 2 * n), 100)
+                rewards = np.empty(NUM)
                 ss = make_adj_set(graph)
                 Timer.start('sample')
                 Counter.count('sample')
-                # ランダムサンプリングはプロセス並列で実行
-                pool = Pool()
-                rewards = np.array(pool.map(randomplay, [ss] * NUM))
-                pool.close()
+                for i in range(NUM):
+                    rewards[i] = randomplay(ss)
                 Timer.end('sample')
                 self.reward_mean = rewards.mean()
                 # stdを0にしないようにEPSを足す
