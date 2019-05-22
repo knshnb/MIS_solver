9f24c258d5aca7638a29e340be56109d73fb63e0
Kenshin Abe <abe.kenshin@gmail.com>
2019-05-23 03:41:31 +0900

    Use model

--- use_model.py
+++ use_model.py
@@ -8,43 +8,55 @@ from mcts.mcts import MCTS
 from gin.gin import GIN3
 from utils.timer import Timer
 
-def best_gin():
+def best_gin1():
     gnn = GIN3(layer_num=2, feature=8)
     gnn.load_state_dict(torch.load("model/train1/save_100_100_2.pth"))
     gnn.to(device)
     gnn.eval()
     return gnn
 
-def use_model(graph, TAU, iter_p=1):
+def best_gin2():
+    gnn = GIN3(layer_num=6, feature=8)
+    gnn.load_state_dict(torch.load("model/decay_train2_p5_0th.pth"))
+    gnn.to(device)
+    gnn.eval()
+    return gnn
+
+def use_model(gnn, name, graph, iter_num=1000):
     # seedを初期化しないと全部同じになってしまう！
     np.random.seed()
 
-    gnn = best_gin()
+    gnn = best_gin2()
     mcts = MCTS(gnn, performance=True)
 
     Timer.start('all')
 
-    result = mcts.search(graph, iter_num=100)
+    result = mcts.search(graph, iter_num=iter_num)
     # result = mcts.best_search2(graph, TAU=TAU, iter_p=iter_p)
     # result = mcts.best_search2(graph)
     # result = mcts.greedy_v_search(graph)
-    print("TAU: {}, result: {}".format(TAU, result))
+    print("graph: {}, result: {}".format(name, result))
 
     Timer.end('all')
     Timer.print()
 
 if __name__ == "__main__":
-    graph0 = read_graph("data/random/1000_2500_0").adj
-    graph1 = read_graph("data/random/100_250_0").adj
-    cora = read_graph("data/cora").adj
-    citeseer = read_graph("data/citeseer").adj
+    graphs = {
+        "100_250_0": read_graph("data/random/100_250_0").adj,
+        "cora": read_graph("data/cora").adj,
+        "siteseer": read_graph("data/citeseer").adj,
+    }
+    gnns = [
+        best_gin1(),
+        best_gin2(),
+    ]
 
-    # print(multiprocessing.cpu_count())
-    # pool = Pool()
-    # for i in range(32):
-    #     TAU = 0.1 + 0.5 * 0.8 ** i
-    #     pool.apply_async(use_model, args=(graph1, TAU))
-    # pool.close()
-    # pool.join()
+    print(multiprocessing.cpu_count())
+    pool = Pool()
+    for name, graph in graphs.items():
+        for gnn in gnns:
+            pool.apply_async(use_model, args=(gnn, name, graph))
+    pool.close()
+    pool.join()
 
-    use_model(cora, 0.3)
+    # use_model("cora", 0.3)
