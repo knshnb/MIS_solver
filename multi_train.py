ebd942066a65700e29a24b394c008f881a18be75
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2019-05-25 10:30:11 +0900

    hoghe

--- multi_train.py
+++ multi_train.py
@@ -16,19 +16,19 @@ def train(idx):
 
     gnn = GIN3(layer_num=6)
     gnn.to(device)
-    trainer = MCTSTrainer(gnn, test_graphs, "decay_train2_p2_{}th".format(idx))
+    trainer = MCTSTrainer(gnn, test_graphs, "decay_joe_test{}th".format(idx))
 
     Timer.start('all')
 
-    for i in range(100):
+    for i in range(200):
         print("epoch: ", i)
-        graph = generate_random_graph(100, 250).adj
+        graph = generate_random_graph(500, 1250).adj
         Timer.start('test')
         trainer.test()
         Timer.end('test')
 
         Timer.start('train')
-        trainer.train2(graph, 10 * 0.96 ** i, iter_p=2)
+        trainer.train2(graph, 10 * 0.98 ** i, iter_p=2)
         Timer.end('train')
 
     Timer.start('test')
